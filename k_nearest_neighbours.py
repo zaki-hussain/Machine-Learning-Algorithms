8e00c4f742775d0fd3e6ae4d1e4701549e2c78a6
Zaki Hussain <mail@zaki.me>
2024-09-04 17:42:26 +0100

    fixed so this class can be used from another file

--- k_nearest_neighbours.py
+++ k_nearest_neighbours.py
@@ -15,25 +15,25 @@ class KNN:
         self.prediction = np.argmax(np.bincount(self.y_train[self.ordered_indices][:self.k]))
         print(self.prediction)
 
+if __name__ == "__main__":
+    df = pd.read_csv("iris.csv")
+    df = df.drop("Id", axis=1)
+    y_train = np.array([], dtype=int)
 
-df = pd.read_csv("iris.csv")
-df = df.drop("Id", axis=1)
-y_train = np.array([], dtype=int)
+    for i in df.index:
+        match df.loc[i, "Species"]:
+            case "Iris-setosa":
+                y_train = np.append(y_train,[0])
+            case "Iris-versicolor":
+                y_train = np.append(y_train,[1])
+            case "Iris-virginica":
+                y_train = np.append(y_train,[2])
 
-for i in df.index:
-    match df.loc[i, "Species"]:
-        case "Iris-setosa":
-            y_train = np.append(y_train,[0])
-        case "Iris-versicolor":
-            y_train = np.append(y_train,[1])
-        case "Iris-virginica":
-            y_train = np.append(y_train,[2])
+    df = df.drop("Species", axis=1)
+    x_train = df.to_numpy()
 
-df = df.drop("Species", axis=1)
-x_train = df.to_numpy()
+    iris_classifier = KNN(int(input("What value should k be?\n")),x_train,y_train)
 
-iris_classifier = KNN(int(input("What value should k be?\n")),x_train,y_train)
+    new_data_point = np.array([float(input("What is the sepal length (cm)?\n")),float(input("What is the sepal width (cm)\n")),float(input("What is the petal length (cm)?\n")),float(input("What is the petal width (cm)?\n"))])
 
-new_data_point = np.array([float(input("What is the sepal length (cm)?\n")),float(input("What is the sepal width (cm)\n")),float(input("What is the petal length (cm)?\n")),float(input("What is the petal width (cm)?\n"))])
-
-iris_classifier.predict(new_data_point)
+    iris_classifier.predict(new_data_point)
